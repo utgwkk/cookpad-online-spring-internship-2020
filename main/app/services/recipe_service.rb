12c6647b3217ea42dce414d1b68ecf3c5444750d
utgwkk <utagawakiki@gmail.com>
2020-04-29 06:22:41 +0000

    avoid N+1 query on list_recipes

--- main/app/services/recipe_service.rb
+++ main/app/services/recipe_service.rb
@@ -15,8 +15,9 @@ class RecipeService < Main::Services::V1::Recipe::Service
     page = request.page unless request.page.zero?
     per_page = request.per_page unless request.per_page.zero?
 
-    # TODO: Avoid to N+1 query
     recipes = Recipe.
+      preload(:user).
+      preload(:steps).
       order(created_at: :desc).
       page(page).
       per(per_page)
